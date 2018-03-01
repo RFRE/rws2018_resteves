55da76861093c63db1ee10e8e568d8ec06f751a3
Ruben Esteves <ruben.esteves@ua.pt>
2018-03-01 14:40:25 +0000

    vectors and stuff

--- player_resteves/src/player_resteves_node.cpp
+++ player_resteves/src/player_resteves_node.cpp
@@ -1,10 +1,23 @@
 #include <iostream>                                                                                     
+#include <vector>
+#include <boost/shared_ptr.hpp>
+
+// include ros libraries
+#include <ros/ros.h>
+
+#include <sstream>
+
+// this way i don't have to write std::something all the time
+using namespace std;
+
+namespace rws_resteves
+{
 
 class Player
 {
     public:
 
-    Player(std::string argin_name)
+    Player(string argin_name)
     {
         name = argin_name;
     }
@@ -26,13 +39,13 @@ class Player
         }
         else
         {
-            std::cout << "Error: invalid color index input" << std::endl;
+            cout << "Error: invalid color index input" << endl;
             return 0;
         }
     }
 
     // Setter for team_name
-    int setTeamName(std::string argin_team)
+    int setTeamName(string argin_team)
     {
         if(argin_team == "red" || argin_team == "green" || argin_team == "blue")
         {
@@ -41,47 +54,110 @@ class Player
         }
         else
         {
-            std::cout << "Error: invalid color input: " << argin_team << std::endl;
+            cout << "Error: invalid color input: " << argin_team << endl;
             return 0;
         }
     }
 
-    std::string name;
-    std::string getTeam(void){return team_name;}
+    string name;
+    string getTeam(void){return team_name;}
 
     private:
 
-    std::string team_name;
+    string team_name;
 
+};
+
+class Team
+{
+    public: 
+      
+       /**
+       * @brief Constructor
+       * @param team_name the team name
+       */
+      Team(string team_name)
+      {
+        name = team_name; 
+
+      }
+
+      /**
+       * @brief Prints the name of the team and the names of all its players
+       */
+      void printTeamInfo(void)
+      {
+        //Write code here ...
+      }
+
+      /**
+       * @brief Checks if a player belongs to the team
+       * @param player_name the name of the player to check
+       * @return true or false, yes or no
+       */
+      bool playerBelongsToTeam(string player_name)
+      {
+        //write code here ...
+      }
+
+      /**
+       * @brief The team name
+       */
+      string name;
+
+      /**
+       * @brief A list of the team's player names
+       */
+      vector<string> players;
 };
 
 class MyPlayer : public Player
 {
     public:
-    MyPlayer(std::string name, std::string team) : Player(name)
+
+    boost::shared_ptr<Team> red_team;
+    boost::shared_ptr<Team> green_team;
+    boost::shared_ptr<Team> blue_team;
+
+    MyPlayer(string name, string team) : Player(name)
     {
+        
         setTeamName(team);
+        printReport();
+    }
+
+    void printReport()
+    {
+        cout << "My name is " << name << " and my team is " << getTeam() << endl;
     }
 };
+
+} // end of namespace
 
 // function overload
 int somar(int a, int b){return a+b;}
 double somar(double a, double b){return a+b;}
 
-int main()
+int main(int argc, char ** argv)
 {
+    ros::init(argc, argv, "resteves");
 
-    std::string player_name = "resteves";
-    //Creating an instance of class Player
-    Player player(player_name);
-    player.setTeamName(2);
-    
+    rws_resteves::MyPlayer my_player("resteves", "red");
+    ros::NodeHandle n;
 
-    std::cout << "Created an instance of class player with public name " << player.name << std::endl;
-    std::cout << "Team is " << player.getTeam() << std::endl;
+    string test_param_value;
+    vector<string> test_red;
 
-    MyPlayer my_player("resteves", "red");
-    std::cout << "Created an instance of class player with public name " << my_player.name << std::endl;
-    std::cout << "Team is " << my_player.getTeam() << std::endl;
+    n.getParam("test_param", test_param_value);
+    n.getParam("team_red", test_red);
 
+    cout << "read test_param with value " << test_param_value << endl;
+
+    for (int x = 0; x != test_red.size(); ++x)
+    {
+        cout << "Player " << x+1 << " name: " << test_red[x] << endl;
+        //cout << test_red.at(x) << "- calling at member" << endl;
+    }
+
+    ros::spin();
 }
