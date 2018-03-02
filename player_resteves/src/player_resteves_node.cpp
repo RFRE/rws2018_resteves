56134dbe1c741f831cd5bf4e06408bbba186dbbf
Ruben Esteves <ruben.esteves@ua.pt>
2018-03-02 11:40:12 +0000

    Linear move by publishing to topic make_a_play with --rate 10

--- player_resteves/src/player_resteves_node.cpp
+++ player_resteves/src/player_resteves_node.cpp
@@ -10,6 +10,7 @@
 
 // other libraries
 #include <rws2018_libs/team.h>
+#include <rws2018_msgs/MakeAPlay.h>
 
 // this way i don't have to write std::something all the time
 using namespace std;
@@ -26,25 +27,15 @@ namespace rws_resteves
                 name = argin_name;
             }
 
+            string name;
+
             //overloaded setter for team_name
             int setTeamName(int index)
             {
-                if(index == 1)
-                {
-                    return setTeamName("red");
-                }
-                else if(index == 2)
-                {
-                    return setTeamName("green");
-                }
-                else if(index == 3)
-                {
-                    return setTeamName("blue");
-                }
-                else
-                {
-                    setTeamName("none");
-                }
+                if(index == 1){return setTeamName("red");}
+                else if(index == 2){return setTeamName("green");}
+                else if(index == 3){return setTeamName("blue");}
+                else{setTeamName("none");}
             }
 
             // Setter for team_name
@@ -58,12 +49,10 @@ namespace rws_resteves
                 else
                 {
                     //cout << "Error: invalid color input: " << argin_team << endl;
-                    ROS_ERROR("Invalid color input: %s", argin_team.c_str());
-                    return 0;
+                    ROS_ERROR("cannot set team name to %s", argin_team.c_str());
+                    ros::shutdown();
                 }
             }
-
-            string name;
 
             // Getter of team_name
             string getTeamName(void){return team_name;}
@@ -79,26 +68,75 @@ namespace rws_resteves
             boost::shared_ptr<Team> red_team;
             boost::shared_ptr<Team> green_team;
             boost::shared_ptr<Team> blue_team;
+            boost::shared_ptr<Team> my_team;
+            boost::shared_ptr<Team> my_preys;
+            boost::shared_ptr<Team> my_hunters;
+
             tf::TransformBroadcaster br; //declare the broadcaster
+            ros::NodeHandle n;
+            boost::shared_ptr<ros::Subscriber> sub;
+            tf::Transform T; //declare the transformation object (player's pose wrt world)
+
 
             MyPlayer(string name, string team) : Player(name)
             {
-               red_team = boost::shared_ptr<Team> (new Team("red"));
+                red_team = boost::shared_ptr<Team> (new Team("red"));
                 green_team = boost::shared_ptr<Team> (new Team("green"));
                 blue_team = boost::shared_ptr<Team> (new Team("blue"));
 
-                setTeamName(team);
+                if (red_team->playerBelongsToTeam(name))
+                {
+                    my_team = red_team;
+                    my_preys = green_team;
+                    my_hunters = blue_team;
+                    setTeamName("red");
+                }
+                else if (green_team->playerBelongsToTeam(name))
+                {
+                    my_team = green_team;
+                    my_preys = blue_team;
+                    my_hunters = red_team;
+                    setTeamName("green");
+                }
+                else if (blue_team->playerBelongsToTeam(name))
+                {
+                    my_team = blue_team;
+                    my_preys = red_team;
+                    my_hunters = green_team;
+                    setTeamName("blue");
+                }
+
+                sub = boost::shared_ptr<ros::Subscriber> (new ros::Subscriber());
+                *sub = n.subscribe("/make_a_play", 100, &MyPlayer::move, this);
+
+                warp(randomizePosition(), randomizePosition(), M_PI/2);
+
                 printReport();
             }
 
-            void move(void)
+            void warp(double x, double y, double alfa)
             {
-                tf::Transform transform; //declare the transformation object
-                transform.setOrigin( tf::Vector3(-5, 7, 0.0) );
+                T.setOrigin( tf::Vector3(x, y, 0.0) );
                 tf::Quaternion q;
-                q.setRPY(0, 0, M_PI/3);
-                transform.setRotation(q);
-                br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), "world", "resteves"));
+                q.setRPY(0, 0, alfa);
+                T.setRotation(q);
+                br.sendTransform(tf::StampedTransform(T, ros::Time::now(), "world", "resteves"));
+                ROS_INFO("Warping to x=%f y=%f a=%f", x,y,alfa);
+            }
+
+            void move(const rws2018_msgs::MakeAPlay::ConstPtr& msg)
+            {
+                double x = T.getOrigin().x();
+                double y = T.getOrigin().y();
+                double a = 0;
+
+                T.setOrigin( tf::Vector3(x+=0.01, y, 0.0) );
+                tf::Quaternion q;
+                q.setRPY(0, 0, a);
+                T.setRotation(q);
+
+                br.sendTransform(tf::StampedTransform(T, ros::Time::now(), "world", "resteves"));
+                ROS_INFO("Moving to ");
             }
 
             void printReport()
@@ -114,6 +152,7 @@ namespace rws_resteves
 int somar(int a, int b){return a+b;}
 double somar(double a, double b){return a+b;}
 
+
 int main(int argc, char ** argv)
 {
     ros::init(argc, argv, "resteves");
@@ -122,6 +161,8 @@ int main(int argc, char ** argv)
 
     rws_resteves::MyPlayer my_player("resteves", "blue");
     
+    //ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+
     // string test_param_value;
     // vector<string> test_red;
 
@@ -136,12 +177,14 @@ int main(int argc, char ** argv)
     //     //cout << test_red.at(x) << "- calling at member" << endl;
     // }
     
-    ros::Rate loop_rate(10);
-    while(ros::ok())
-    {
-        my_player.move();
+    // ros::Rate loop_rate(10);
+    // while(ros::ok())
+    // {
+    //     my_player.move();
 
-        ros::spinOnce();
-        loop_rate.sleep();
-    }
+    //     ros::spinOnce();
+    //     loop_rate.sleep();
+    // }
+
+    ros::spin();
 }
