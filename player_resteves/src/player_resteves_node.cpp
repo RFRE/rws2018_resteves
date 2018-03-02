25d841e0610ead79f4c05b132cd1e93910b2b955
Ruben Esteves <ruben.esteves@ua.pt>
2018-03-02 14:09:49 +0000

    Added constraints

--- player_resteves/src/player_resteves_node.cpp
+++ player_resteves/src/player_resteves_node.cpp
@@ -81,69 +81,92 @@ namespace rws_resteves
             MyPlayer(string name, string team) : Player(name)
             {
                 red_team = boost::shared_ptr<Team> (new Team("red"));
-                green_team = boost::shared_ptr<Team> (new Team("green"));
-                blue_team = boost::shared_ptr<Team> (new Team("blue"));
+      green_team = boost::shared_ptr<Team> (new Team("green"));
+      blue_team = boost::shared_ptr<Team> (new Team("blue"));
 
-                if (red_team->playerBelongsToTeam(name))
-                {
-                    my_team = red_team;
-                    my_preys = green_team;
-                    my_hunters = blue_team;
-                    setTeamName("red");
-                }
-                else if (green_team->playerBelongsToTeam(name))
-                {
-                    my_team = green_team;
-                    my_preys = blue_team;
-                    my_hunters = red_team;
-                    setTeamName("green");
-                }
-                else if (blue_team->playerBelongsToTeam(name))
-                {
-                    my_team = blue_team;
-                    my_preys = red_team;
-                    my_hunters = green_team;
-                    setTeamName("blue");
-                }
+      if (red_team->playerBelongsToTeam(name))
+      {
+        my_team = red_team;
+        my_preys = green_team;
+        my_hunters = blue_team;
+        setTeamName("red");
+      }
+      else if (green_team->playerBelongsToTeam(name))
+      {
+        my_team = green_team;
+        my_preys = blue_team;
+        my_hunters = red_team;
+        setTeamName("green");
+      }
+      else if (blue_team->playerBelongsToTeam(name))
+      {
+        my_team = blue_team;
+        my_preys = red_team;
+        my_hunters = green_team;
+        setTeamName("blue");
+      }
 
-                sub = boost::shared_ptr<ros::Subscriber> (new ros::Subscriber());
-                *sub = n.subscribe("/make_a_play", 100, &MyPlayer::move, this);
+      sub = boost::shared_ptr<ros::Subscriber> (new ros::Subscriber());
+      *sub = n.subscribe("/make_a_play", 100, &MyPlayer::move, this);
 
-                warp(randomizePosition(), randomizePosition(), M_PI/2);
+      struct timeval t1;
+      gettimeofday(&t1, NULL);
+      srand(t1.tv_usec);
+      double start_x = ((double)rand()/(double)RAND_MAX) *10 -5;
+      double start_y = ((double)rand()/(double)RAND_MAX) *10 -5;
+      printf("start_x=%f start_y=%f\n", start_x, start_y);
+      warp(start_x, start_y, M_PI/2);
 
-                printReport();
-            }
+      printReport();
+    }
 
-            void warp(double x, double y, double alfa)
-            {
-                T.setOrigin( tf::Vector3(x, y, 0.0) );
-                tf::Quaternion q;
-                q.setRPY(0, 0, alfa);
-                T.setRotation(q);
-                br.sendTransform(tf::StampedTransform(T, ros::Time::now(), "world", "resteves"));
-                ROS_INFO("Warping to x=%f y=%f a=%f", x,y,alfa);
-            }
+      void warp(double x, double y, double alfa)
+      {
+        T.setOrigin( tf::Vector3(x, y, 0.0) );
+        tf::Quaternion q;
+        q.setRPY(0, 0, alfa);
+        T.setRotation(q);
+        br.sendTransform(tf::StampedTransform(T, ros::Time::now(), "world", "resteves"));
+        ROS_INFO("Warping to x=%f y=%f a=%f", x,y,alfa);
+      }
 
-            void move(const rws2018_msgs::MakeAPlay::ConstPtr& msg)
-            {
-                double x = T.getOrigin().x();
-                double y = T.getOrigin().y();
-                double a = 0;
+      void move(const rws2018_msgs::MakeAPlay::ConstPtr& msg)
+      {
+        double x = T.getOrigin().x();
+        double y = T.getOrigin().y();
+        double a = 0;
 
-                T.setOrigin( tf::Vector3(x+=0.01, y, 0.0) );
-                tf::Quaternion q;
-                q.setRPY(0, 0, a);
-                T.setRotation(q);
+        //---------------------------------------
+        //--- AI PART 
+        //---------------------------------------
+        double displacement = 6; //computed using AI
+        double delta_alpha = M_PI/2;
 
-                br.sendTransform(tf::StampedTransform(T, ros::Time::now(), "world", "resteves"));
-                ROS_INFO("Moving to ");
-            }
 
-            void printReport()
-            {
-                //cout << "My name is " << name << " and my team is " << getTeamName() << endl;
-                ROS_INFO("Player %s is on the %s team.", name.c_str(), getTeamName().c_str());
-            }
+        //---------------------------------------
+        //--- CONSTRAINS PART 
+        //---------------------------------------
+        double displacement_max = msg->dog;
+        displacement > displacement_max ? displacement = displacement_max: displacement = displacement;
+
+        double delta_alpha_max = M_PI/30;
+        fabs(delta_alpha) > fabs(delta_alpha_max) ? delta_alpha = delta_alpha_max * delta_alpha / fabs(delta_alpha): delta_alpha = delta_alpha;
+
+        tf::Transform my_move_T; //declare the transformation object (player's pose wrt world)
+        my_move_T.setOrigin( tf::Vector3(displacement, 0.0, 0.0) );
+        tf::Quaternion q1;
+        q1.setRPY(0, 0, delta_alpha);
+        my_move_T.setRotation(q1);
+ 
+        T = T * my_move_T;
+        br.sendTransform(tf::StampedTransform(T, ros::Time::now(), "world", "resteves"));
+
+      }
+
+      void printReport()
+      {
+        ROS_INFO("My name is %s and my team is %s", name.c_str(), (getTeamName().c_str()) );
+      }
     };
 
 } // end of namespace
